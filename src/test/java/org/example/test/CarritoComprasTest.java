fa1d4a07e37ae83d22c870f33479935416e0a3e1
juliscrep <julietascrepnik@gmail.com>
2024-04-06 20:10:58 -0300

    se crean tests compraSinApellidoYCPDeCliente y compraSinCodigoPostalDeCliente

--- src/test/java/org/example/test/CarritoComprasTest.java
+++ src/test/java/org/example/test/CarritoComprasTest.java
@@ -301,8 +301,110 @@ public class CarritoComprasTest {
             System.out.println("No se pudo iniciar sesion!");
         }
 
-
     }
+
+    @Test
+    public void compraSinApellidoYCPDeCliente(){
+
+        //configuracion del driver
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\test\\resources\\webdriver\\chromedriver.exe");
+
+        // creacion del driver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com/");
+
+        //Localizacion del elemento
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        // Espera explícita
+        WebDriverWait wait1 = new WebDriverWait(driver,Duration.ofSeconds(15));
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.className("app_logo")));
+
+        if(driver.findElement(By.className("app_logo")).isDisplayed()){
+
+            List<WebElement> productos = driver.findElements(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']"));
+
+            for (WebElement producto : productos) {
+                producto.click();
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+            }
+
+            driver.findElement(By.className("shopping_cart_link")).click();
+            driver.findElement(By.id("checkout")).click();
+
+            if(driver.findElement(By.className("title")).isDisplayed()){
+                driver.findElement(By.id("first-name")).sendKeys("Juan");
+                driver.findElement(By.id("continue")).click();
+
+                WebElement mensajeError= driver.findElement(By.xpath("//h3[@data-test='error']"));
+                Assert.assertEquals(mensajeError.getText(),"Error: Last Name is required");
+
+            }
+            else {
+                System.out.println("No se pueden visualizar los productos del carrito!");
+            }
+
+        }
+        else{
+            System.out.println("No se pudo iniciar sesion!");
+        }
+    }
+
+    @Test
+    public void compraSinCodigoPostalDeCliente(){
+
+        //configuracion del driver
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\test\\resources\\webdriver\\chromedriver.exe");
+
+        // creacion del driver
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com/");
+
+        //Localizacion del elemento
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        // Espera explícita
+        WebDriverWait wait1 = new WebDriverWait(driver,Duration.ofSeconds(15));
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.className("app_logo")));
+
+        if(driver.findElement(By.className("app_logo")).isDisplayed()){
+
+            List<WebElement> productos = driver.findElements(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']"));
+
+            for (WebElement producto : productos) {
+                producto.click();
+                new WebDriverWait(driver, Duration.ofSeconds(15));
+            }
+
+            driver.findElement(By.className("shopping_cart_link")).click();
+            driver.findElement(By.id("checkout")).click();
+
+            if(driver.findElement(By.className("title")).isDisplayed()){
+                driver.findElement(By.id("first-name")).sendKeys("Juan");
+                driver.findElement(By.id("last-name")).sendKeys("Perez");
+                driver.findElement(By.id("continue")).click();
+
+                WebElement mensajeError= driver.findElement(By.xpath("//h3[@data-test='error']"));
+                Assert.assertEquals(mensajeError.getText(),"Error: Postal Code is required");
+
+            }
+            else {
+                System.out.println("No se pueden visualizar los productos del carrito!");
+            }
+        }
+        else{
+            System.out.println("No se pudo iniciar sesion!");
+        }
+    }
+
 
 }
 
